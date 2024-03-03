dd2b79bb0cb7552afc40d956250cb7c8ab93781f
Royal O <royalsylvester@gmail.com>
2024-03-03 01:22:49 -0500

    Updated my JavaScript to accommodate all kinds of emails

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function validateEmail(){
         emailError.innerHTML = 'Email is required'
         return false;
     }
-    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    if(!email.match(/^.+@.+\..+$/)){
         emailError.innerHTML = 'Invalid Email'
         return false;
     }
